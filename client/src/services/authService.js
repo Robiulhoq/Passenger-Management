3082c9baecb93a1999a6aa7f9d6a66c80b58ab74
Robiulhoq <robiulhoqrakibjohan@gmail.com>
2026-02-09 21:21:49 +0600

    auth problem fix

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-const API_BASE = `${API_URL}/auth`;
+const API_BASE = "/api/auth";
 
 const authService = {
   login: async (userName, password) => {
